670d41f882690ffabab54aaf5ef99f1717978879
Greg Palmier <gregpalmier@gmail.com>
2014-01-07 15:46:34 -0600

    update email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'promet_php'
 maintainer       'Promet Solutions, Inc.'
-maintainer_email 'greg@promethost.com'
+maintainer_email 'greg@prometsource.com'
 license          'All rights reserved'
 description      'Installs/Configures promet_php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
