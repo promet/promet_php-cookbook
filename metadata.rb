738bafad35626205a5987bf9b81619dcc3977ce2
Greg Palmier <gregpalmier@gmail.com>
2014-02-05 20:10:53 -0600

    update apache2 typo uncovered during berks upload.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures promet_php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
-depends		 'apache'
+depends		 'apache2'
 depends		 'apt'
 depends		 'php', '= 1.3.10'
